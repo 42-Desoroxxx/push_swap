6aaaabf5b4eca014c2a648dc4216c7434b9f9800
Luna Mira Lage (Desoroxxx) <desoroxxx.git@redstudio.dev>
2025-04-06 00:47:01 +0200

    meow

--- src/utils/algorithm_utils.c
+++ src/utils/algorithm_utils.c
@@ -15,7 +15,7 @@
 
 void	bring_to_top(struct s_stack *stack, const long long value)
 {
-	size_t index;
+	size_t	index;
 
 	index = get_index(stack, value);
 	while (stack->values[stack->size - 1] != value)
@@ -23,20 +23,20 @@ void	bring_to_top(struct s_stack *stack, const long long value)
 		if (index > (stack->size - 1) / 2)
 			rotate(stack);
 		else
-			reverse_rotate	(stack);
+			reverse_rotate (stack);
 	}
 }
 
-bool is_sorted(const struct s_stack *stack)
+bool	is_sorted(const struct s_stack *stack)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i < stack->size - 1)
 	{
 		if (stack->values[i] < stack->values[i + 1])
-			return false;
+			return (false);
 		i++;
 	}
-	return true;
+	return (true);
 }
